30789a9491c137016554d2669eea886f8996ec48
jlarrieux <jeanrodneylarrieux@gmail.com>
2018-09-28 22:16:32 -0500

    initial commit

---
+++ src/main/java/com/jlarrieux/learning/udemy/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,17 @@
+package com.jlarrieux.learning.udemy.sfgpetclinic;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jlarrieux/learning/udemy/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,21 @@
+package com.jlarrieux.learning.udemy.sfgpetclinic;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgPetClinicApplicationTests {
+
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
